4eb099e9d680e8c85bd06d6166052c112f130ed8
thenappan.2010@gmail.com <Thenappan.Ramanathan@walmart.com>
2021-11-29 22:11:36 +0530

    bg button and flow added

--- MergeTestTwo/MergeTestTwo/ViewController.swift
+++ MergeTestTwo/MergeTestTwo/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let centerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Change background color", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor.systemTeal
+        return button
+    }()
+
+    var bgValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(centerButton)
+        centerButton.translatesAutoresizingMaskIntoConstraints = false
+        centerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        centerButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        centerButton.addTarget(self, action: #selector(setBgColor), for: .touchUpInside)
     }
 
-
+    @objc func setBgColor() {
+        switch bgValue {
+        case 0:
+            self.view.backgroundColor = UIColor.red
+        case 1:
+            self.view.backgroundColor = UIColor.blue
+        case 2:
+            self.view.backgroundColor = UIColor.green
+        case 3:
+            self.view.backgroundColor = UIColor.gray
+        case 4:
+            self.view.backgroundColor = UIColor.brown
+        default:
+            self.view.backgroundColor = UIColor.white
+        }
+        bgValue += 1
+        bgValue = bgValue%6
+    }
 }
 
